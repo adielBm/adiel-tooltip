f532a8470ebc5d1587f9e58355c95b2e805b4181
adiel <adiel.bm5@gmail.com>
2021-10-30 22:00:52 +0300

    some update.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css'; // optional for styling
 
+document.querySelectorAll('.the-content a').forEach( el => {
+
+  const href = el.href
+
+  if ( href.incloud('glossary') == false ) return;
+
+  tippy(el, {
+    content: 'טוען...',
+    onCreate(instance) {
+      // Setup our own custom state properties
+      instance._isFetching = false;
+      instance._src = null;
+      instance._error = null;
+    },
+    onShow(instance) {
+      if (instance._isFetching || instance._src || instance._error) {
+        return;
+      }
+      instance._isFetching = true;
+
+      const data = new FormData();
+      data.append('postUrl', href);
+  
+      fetch(adiel_tooltip.ajaxurl, {
+        body: data,
+        method: 'POST'
+      })
+        .then( response => response.json())
+        .then( response => {
+          instance.setContent(`<h4>${response.post_title}</h4><span>${response.post_content}</span>`);
+        })
+        .catch((error) => {
+          instance._error = error;
+          instance.setContent(`Request failed. ${error}`);
+        })
+        .finally(() => {
+          instance._isFetching = false;
+        });
+    },
+    onHidden(instance) {
+      instance.setContent('טוען...');
+      // Unset these properties so new network requests can be initiated
+      instance._src = null;
+      instance._error = null;
+    },
+  });
+})
+
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
+
